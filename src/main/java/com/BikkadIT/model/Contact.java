b160de124d3cb7ee198171c57add06306392cb55
dorkhebharat444 <dorkhebharat444@gmail.com>
2022-08-18 03:07:54 -0700

    Poroject packeg complition

---
+++ src/main/java/com/BikkadIT/model/Contact.java
@@ -0,0 +1,16 @@
+package com.BikkadIT.model;
+
+public class Contact {
+
+	
+	 private  Integer contactId;
+	 
+	 private  String  contactname;
+	 
+	 private String  contcatemail;
+	 
+	 private  char  activeswitech;
+	 
+	
+	
+}
